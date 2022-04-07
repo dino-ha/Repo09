279f1cec4345b2f67070437bd5e685e3f115edbd
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:47:38 +0200

    Add files via upload

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,1 +1,15 @@
+function functionElse(num){
 
+    if (num <= 5){
+
+        return "5 or smaller";
+    }
+    else {
+
+        return "Bigger than 5";
+    }
+}
+
+functionElse(10);
+
+module.exports = functionElse;
