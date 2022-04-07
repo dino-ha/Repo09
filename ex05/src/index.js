e0e83c36ac82335af327fa37db50022b1073b649
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:44:21 +0200

    Add files via upload

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,1 +1,12 @@
+function compareStrictValues(m){
 
+    if (m != 55){
+
+        return "Not equal";
+    }
+    return "Equal";
+}
+
+compareStrictValues(10);
+
+module.exports = compareStrictValues;
