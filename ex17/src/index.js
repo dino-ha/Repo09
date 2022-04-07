c7050fef9ebf89a7360c506c00a4585c7fc0979d
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:51:19 +0200

    Add files via upload

--- ex17/src/index.js
+++ ex17/src/index.js
@@ -1,1 +1,20 @@
+function inverseWhile(){
 
+    var fiveNumbers = "";
+    var stringNumber = 5;
+    while (stringNumber >= 0){
+
+        if (stringNumber == 0){
+
+            fiveNumbers += stringNumber;
+        }
+        
+        fiveNumbers += stringNumber + ",";
+        stringNumber--;
+    }
+    return fiveNumbers;
+}
+
+inverseWhile();
+
+module.exports = inverseWHile;
