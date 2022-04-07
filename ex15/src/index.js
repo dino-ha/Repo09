cd57d92c26e52ad4fea5b36b4443ccd023b7b496
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:50:18 +0200

    Add files via upload

--- ex15/src/index.js
+++ ex15/src/index.js
@@ -1,1 +1,23 @@
+function switchCase(letter){
 
+    switch (letter){
+        case "a":
+            "antelope";
+            break;
+        case "b":
+            "bird";
+            break;
+        case "c":
+            "cat";
+            break;
+        default:
+            "stuff";
+            break;
+    }
+
+    return letter;
+}
+
+switchCase("c");
+
+module.exports = switchCase;
