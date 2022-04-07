a57aaa8670120b4cf0e11f5b28fc78fcc3f859b2
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:51:51 +0200

    Add files via upload

--- ex18/src/index.js
+++ ex18/src/index.js
@@ -1,1 +1,46 @@
+function myForLoop1(){
 
+    var evenNumbers = "";
+    for (var i = 0; i <= 8; i++){
+
+        if (i == 8 && i % 2 == 0){
+
+            evenNumbers += i;
+        }
+        
+        if (i % 2 == 0){
+            evenNumbers += i + ",";
+        }
+
+    }
+    return evenNumbers;
+}
+
+function myForLoop2(){
+
+    var evenInverseNumbers = "";
+    for (var i = 8; i >= 0; i--){
+
+        if (i == 0 && i % 2 == 0){
+
+            evenInverseNumbers += i;
+        }
+
+        if (i % 2 == 0){
+
+            evenInverseNumbers += i + ",";
+        }
+    }
+    return evenInverseNumbers;
+
+
+}
+
+myForLoop1();
+myForLoop2();
+
+module.exports = {
+
+    myForLoop1,
+    myForLoop2
+};
