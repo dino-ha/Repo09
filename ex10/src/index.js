f9f81cc8cdf2a30d6ce95b3099efb90d80326620
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:47:03 +0200

    Add files via upload

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,1 +1,14 @@
+function mathTest(x, y){
 
+    if (x < 0 || y < 0){
+
+        return;
+    }
+
+    return Math.round(Math.pow(Math.sqrt(x)+Math.sqrt(y), 2));
+}
+
+mathTest(-2, 3);
+mathTest(15, 20);
+
+module.exports = mathTest;
