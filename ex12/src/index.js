6f134baf93f89f0794754c9f3d8a09aa41807f3c
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:48:08 +0200

    Add files via upload

--- ex12/src/index.js
+++ ex12/src/index.js
@@ -1,1 +1,19 @@
+function logicalOrder(num){
 
+    if (num < 50){
+
+        return "Less than 50";
+    }
+    else if (num < 100){
+
+        return "Less than 100";
+    }
+    else {
+
+        return "Greater than or equal to 100";
+    }
+}
+
+logicalOrder(100);
+
+module.exports = logicalOrder;
