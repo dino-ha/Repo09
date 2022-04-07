4552f59bedf7d28eeac549d62426507636da1d0b
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:44:57 +0200

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,22 @@
+function comparisonToEqual(a){
 
+    if (a < 5){
+
+        return "Less than 5";
+    }
+    if (a < 10){
+
+        return "Less than 10";
+    }
+    if (a >= 10){
+
+        return "10 or over";
+    }
+    if (a > 20){
+        return "More than 20";
+    }
+}
+
+comparisonToEqual(23);
+
+module.exports = comparisonToEqual;
