1d58649cf4f45e5fe16bb1a8e8a1cfd15a529203
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:45:59 +0200

    Add files via upload

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,1 +1,12 @@
+function logicalAndOperator(num){
 
+    if (num <= 80 && num >= 40){
+
+        return "Yes";
+    }
+    return "No";
+}
+
+logicalAndOperator(50);
+
+module.exports = logicalAndOperator;
