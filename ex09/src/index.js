7977707f746eae36a5611f58837c8633e397f618
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:46:33 +0200

    Add files via upload

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,1 +1,12 @@
+function logicalOrOperator(num){
 
+    if (num <= 20 || num >= 30){
+
+        return "Out";
+    }
+    return "In";
+}
+
+logicalOrOperator(15);
+
+module.exports = logicalOrOperator;
