365bc11f16b2379cfca93c8f127008dc527669f6
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:43:43 +0200

    Add files via upload

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,14 @@
+function compareDifferentValues(m, n){
 
+    if (m === n){
+
+        return "Equal";
+    }
+    return "Not equal";
+}
+
+compareDifferentValues(1, "1");
+compareDifferentValues(1, 1);
+compareDifferentValues(m, n);
+
+module.exports = compareDifferentValues;
