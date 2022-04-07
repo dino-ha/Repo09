65f89b081fb46a30195ed39678c227d671b6246d
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:43:13 +0200

    Add files via upload

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,1 +1,13 @@
+function myEqualFunction(num){
 
+    if (num === 23){
+
+        return "Equal";
+    }
+    return "Not equal";
+}
+
+myEqualFunction(35);
+myEqualFunction();
+
+module.exports = myEqualFunction;
