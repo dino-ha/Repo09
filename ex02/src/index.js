24f218ca00fea07fcd649862e38ca47d5d911df3
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:42:42 +0200

    Add files via upload

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,1 +1,13 @@
+function myFunction(myParameter){
 
+    if(myParameter){
+
+        return "The parameter is true!";
+    }
+    return "The parameter is false";
+}
+
+myFunction(true);
+myFunction(myParameter);
+
+module.exports = myFunction;
