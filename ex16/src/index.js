cebec16a4fe5da344066624eb3df0fdb0fb48814
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:50:49 +0200

    Add files via upload

--- ex16/src/index.js
+++ ex16/src/index.js
@@ -1,1 +1,27 @@
+function switchCaseSequential(num){
+
+    switch(num){
+
+        case 1:
+        case 2:
+        case 3:
+            answer="Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer="Mid"
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer="High";
+            break;
+        default:
+            answer="It's outside the 1-9 range"
+    }
+    return answer;
+}
+
+switchCaseSequential(7);
 
