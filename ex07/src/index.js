dc802546ba273be2aa00841afb4786fb111f2613
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:45:29 +0200

    Add files via upload

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,18 @@
+function randomRangeNumber(minNumber, maxNumber){
 
+    if (minNumber > maxNumber){
+
+        maxTemp = maxNumber;
+        maxNumber = minNumber;
+        minNumber = maxTemp;
+    }
+
+     
+    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+    
+}
+
+randomRangeNumber(12, 20);
+randomRangeNumber(50, 30);
+
+module.exports = randomRangeNumber;
