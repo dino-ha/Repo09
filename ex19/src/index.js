90c8379eb949e48501342b06a63015ca220349ed
dino-ha <hadzirovicdino@gmail.com>
2022-04-07 02:52:21 +0200

    Add files via upload

--- ex19/src/index.js
+++ ex19/src/index.js
@@ -1,1 +1,19 @@
+function myDoWhile(){
 
+    var myNumbers = "";
+    num = 0;
+    do {
+
+        if (num == 9){
+
+            myNumbers += num;
+        }
+        myNumbers += num + ",";
+        num++
+    } while (num < 10)
+
+}
+
+myDoWhile();
+
+module.exports = myDoWhile;
